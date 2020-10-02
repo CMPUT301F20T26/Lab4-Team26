47d0486ac117a6578a190cf9641e1f2d8296ec35
Olumide Okubanjo <olumide.okubanjo@gmail.com>
2020-10-01 21:18:39 -0600

    color field added

--- app/src/main/java/com/example/lab4_team26/Star.java
+++ app/src/main/java/com/example/lab4_team26/Star.java
@@ -1,10 +1,11 @@
 package com.example.lab4_team26;
 
 public class Star extends Shape{
+    private String color = "blue";
 
     //Just a filler
     public Star(){
         super();
     }
-    
+
 }

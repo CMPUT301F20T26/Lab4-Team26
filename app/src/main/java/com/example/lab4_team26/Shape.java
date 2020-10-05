d7ce0e46347dc6f7ba45d3bba3d8ba00061f082d
Olumide Okubanjo <olumide.okubanjo@gmail.com>
2020-10-05 11:37:01 -0600

    Olumide Color added

--- app/src/main/java/com/example/lab4_team26/Shape.java
+++ app/src/main/java/com/example/lab4_team26/Shape.java
@@ -1,7 +1,7 @@
 package com.example.lab4_team26;
 
 public abstract class Shape {
-    protected String color = "red"; 
+    protected String color = "blue";
     
     private int x;
     private int y;
